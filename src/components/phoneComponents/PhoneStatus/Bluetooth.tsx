a0267479b7ed68415238a311806ba3c1e4ba436f
alexis-opolka <alexis.opolka@proton.me>
2023-08-07 23:43:16 +0200

    Adress #14

---
+++ src/components/phoneComponents/PhoneStatus/Bluetooth.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@primer/react";
+import { MdOutlineBluetooth } from "react-icons/md";
+
+export default function BluetoothStatus(){
+  return(
+    <Box>
+      <MdOutlineBluetooth />
+    </Box>
+  )
+}
