f3b22e2ba5e5223ad418b58cc9b89239e3773054
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2023-08-07 23:10:22 +0200

    Add Phone Components

--- src/components/phoneComponents/PhoneComponent.tsx
+++ src/components/phoneComponents/PhoneComponent.tsx
@@ -1,9 +1,7 @@
-import { Box, PageLayout } from "@primer/react";
-import PortfolioHeader from '../PortfolioHeader';
+import { Box } from "@primer/react";
 import PhoneContext from "./PhoneContext";
 import PhoneMockup from "@/content/img/Samsung Galaxy S21 5G — Black — Shadow.png"
 import Image from "next/image";
-import { relative } from "path";
 
 export default function PhoneComponent(){
   return(
@@ -12,6 +10,7 @@ export default function PhoneComponent(){
       textAlign: "center",
       userSelect: "none",
     }}>
+      {/* Mockup phone retrieved from Meta's style & design resources */}
       <Image src={PhoneMockup} alt="Samsung Galaxy S21 5G - Black" height={800} width={450} style={{zIndex: 10, width: "100%", position: "relative"}} draggable={false}/>
       <PhoneContext/>
     </Box>
