7d62da97a8dbd6c64283c2a8f18ee424c3724bf3
alexis-opolka <alexis.opolka@proton.me>
2023-09-04 08:56:34 +0200

    Add QA Testing part and Others section

--- src/components/AboutComponents/Investment/Investment.tsx
+++ src/components/AboutComponents/Investment/Investment.tsx
@@ -29,6 +29,7 @@ export default function MyInvestment(){
 
     const investmentInConference: investmentEventInterface[] = [];
     const investmentInSchoolEvents: investmentEventInterface[] = [];
+    const investmentUnrelatedButInteresting: investmentEventInterface[] = [];
 
     // We preload the content on different lists so that we can easily map them to the cards
     InvestmentData.content.map((investmentEvent, investmentIndex) => {
@@ -38,6 +39,8 @@ export default function MyInvestment(){
         } else if (investmentEvent.category === "school-event"){
             // We add the investmentEvent object to the investmentInSchoolEvents list
             investmentInSchoolEvents.push(investmentEvent);
+        } else if (investmentEvent.category === "other"){
+            investmentUnrelatedButInteresting.push(investmentEvent);
         } else {
             // We shouldn't be here, so we just throw a log with the content
             console.log("We couldn't find the category for this investment event: ", investmentEvent, `with the index n°${investmentIndex}!`)
@@ -52,45 +55,68 @@ export default function MyInvestment(){
                     <Heading as={"h3"} sx={{fontSize: 4, mr: 4}}> My Investment </Heading>
                 </Link>
             </Box>
-            <Box id="About.Investment.Content.Experience" sx={{display: "flex", flexDirection: "row", justifyContent: "space-between", maxWidth: "100%", width: "100%"}}>
-                <Box id="About.Investment.Content.Experience.Conferences" sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+            <Box id="About.Investment.Content.Experience.Holder" sx={{display: "flex", flexDirection: "column", maxWidth: "100%", width: "100%"}}>
+                <Box id="About.Investment.Content.Experience" sx={{display: "flex", flexDirection: "row", justifyContent: "space-between", maxWidth: "100%", width: "100%"}}>
+                    <Box id="About.Investment.Content.Experience.Conferences" sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
 
-                    <Box id="About.Investment.Content.Experience.Conferences.Headings">
-                        <Heading as={"h4"} sx={{fontSize: 3, ml: 4, mt: 4}}>
-                            Conferences
+                        <Box id="About.Investment.Content.Experience.Conferences.Headings">
+                            <Heading as={"h4"} sx={{fontSize: 3, ml: 4, mt: 4}}>
+                                Conferences
 
-                            <Text as={"p"} sx={{fontSize: 1, color: "fg.subtle", mt: 2, mb: 2, ml: 4, mr: 4}}>
-                                I attended the following conferences, either digitally or physically.
-                            </Text>
-                        </Heading>
+                                <Text as={"p"} sx={{fontSize: 1, color: "fg.subtle", mt: 2, mb: 2, ml: 4, mr: 4}}>
+                                    I attended the following conferences, either digitally or physically.
+                                </Text>
+                            </Heading>
+                        </Box>
+
+                        <Box id="About.Investment.Content.Experience.Conferences.Content" sx={{mt: 2, mb: 2, ml: 4, mr: 4}}>
+                            {
+                                investmentInConference.map((investmentEvent, investmentIndex) => {
+                                    return(
+                                        <InvestmentCard key={investmentIndex} id="About.Investment.Content.Experience.Conferences.Content" type={investmentEvent.category} content={investmentEvent} />
+                                    )
+                                })
+                            }
+                        </Box>
                     </Box>
+                    <Box id="About.Investment.Content.Experience.School-Events" sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+                        <Box id="About.Investment.Content.Experience.School-Events.Headings">
+                            <Heading as={"h4"} sx={{fontSize: 3, ml: 4, mt: 4}}>
+                                School Events
 
-                    <Box id="About.Investment.Content.Experience.Conferences.Content" sx={{mt: 2, mb: 2, ml: 4, mr: 4}}>
-                        {
-                            investmentInConference.map((investmentEvent, investmentIndex) => {
-                                return(
-                                    <InvestmentCard key={investmentIndex} id="About.Investment.Content.Experience.Conferences.Content" type={investmentEvent.category} content={investmentEvent} />
-                                )
-                            })
-                        }
+                                <Text as={"p"} sx={{fontSize: 1, color: "fg.subtle", mt: 2, mb: 2, ml: 4, mr: 4}}>
+                                    I either participated or helped organize the following school events.
+                                </Text>
+                            </Heading>
+                        </Box>
+
+                        <Box id="About.Investment.Content.Experience.School-Events.Content" sx={{mt: 2, mb: 2, ml: 4, mr: 4}}>
+                            {
+                                investmentInSchoolEvents.map((investmentEvent, investmentIndex) => {
+                                    return(
+                                        <InvestmentCard key={investmentIndex} id="About.Investment.Content.Experience.School-Events.Content" type={investmentEvent.category} content={investmentEvent} />
+                                    )
+                                })
+                            }
+                        </Box>
                     </Box>
                 </Box>
-                <Box id="About.Investment.Content.Experience.School-Events" sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-                    <Box id="About.Investment.Content.Experience.School-Events.Headings">
+                <Box id="About.Investment.Content.Experience.Others" sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+                    <Box id="About.Investment.Content.Experience.Others.Headings">
                         <Heading as={"h4"} sx={{fontSize: 3, ml: 4, mt: 4}}>
-                            School Events
+                            Others
 
                             <Text as={"p"} sx={{fontSize: 1, color: "fg.subtle", mt: 2, mb: 2, ml: 4, mr: 4}}>
-                                I either participated or helped organize the following school events.
+                                It&apos;s not really related to anything but I thought it would be nice to show it.
                             </Text>
                         </Heading>
                     </Box>
 
-                    <Box id="About.Investment.Content.Experience.School-Events.Content" sx={{mt: 2, mb: 2, ml: 4, mr: 4}}>
+                    <Box id="About.Investment.Content.Experience.Others.Content" sx={{mt: 2, mb: 2, ml: 4, mr: 4}}>
                         {
-                            investmentInSchoolEvents.map((investmentEvent, investmentIndex) => {
+                            investmentUnrelatedButInteresting.map((investmentEvent, investmentIndex) => {
                                 return(
-                                    <InvestmentCard key={investmentIndex} id="About.Investment.Content.Experience.School-Events.Content" type={investmentEvent.category} content={investmentEvent} />
+                                    <InvestmentCard key={investmentIndex} id="About.Investment.Content.Experience.Others.Content" type={investmentEvent.category} content={investmentEvent} />
                                 )
                             })
                         }
